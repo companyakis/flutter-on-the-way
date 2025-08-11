40350ef494b893058b4d3fa0b94718bfb3e805e2
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-08-11 22:52:09 +0300

    Create part-z-0001-basic-types1.dart

---
+++ part-z-0001-basic-types1.dart
@@ -0,0 +1,19 @@
+void main() {
+  
+  int year = 2025;
+  
+  int age = 99;
+  
+  int diff = -21;
+  
+  double exactPrice = 22.45;
+  
+  double pi = 3.14;
+  
+  print(year);
+  print(age);
+  print(diff);
+  print(exactPrice);
+  print(pi);
+
+}

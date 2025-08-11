1325194504e8eeb48f158152535ae6a1bc4f2258
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-08-11 23:19:40 +0300

    Create part-z-0003-type-conversion2.dart

---
+++ part-z-0003-type-conversion2.dart
@@ -0,0 +1,14 @@
+void main() {
+  
+  double price = 22.47;
+  
+  print("Price is $price");
+  
+  int price2 = price.toInt();
+   
+  print("Price is $price2");
+
+}
+
+// Price is 22.47
+// Price is 22

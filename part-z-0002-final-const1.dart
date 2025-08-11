84514a1a38147df58f52549dfb3b10aefe32d9e5
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-08-11 23:04:42 +0300

    Create part-z-0002-final-const1.dart

---
+++ part-z-0002-final-const1.dart
@@ -0,0 +1,11 @@
+void main() {
+  
+  final String secretSentence = "Out of sight, out of mind!";
+  
+  secretSentence = "No!";
+  
+}
+  
+// main.dart:4:3: Error: Can't assign to the final variable 'secretSentence'.
+//   secretSentence = "No!";
+//   ^^^^^^^^^^^^^^

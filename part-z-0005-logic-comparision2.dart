6250127e57d94760cce3c72d3a79d905e4e28e56
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-08-12 10:28:36 +0300

    Create part-z-0005-logic-comparision2.dart

---
+++ part-z-0005-logic-comparision2.dart
@@ -0,0 +1,28 @@
+void main() {
+  
+  int x = 10;
+
+  int y = -5;
+
+  int z = 4;
+
+  print(x == y);
+
+  print(x != z);
+
+  print(x >= z);
+
+  print(z < y);
+
+  print(x > y && z <= y);
+
+  print(y >= x || (x != y && y < z));
+}
+
+
+// false
+// true
+// true
+// false
+// false
+// true

053b040577561a72af0ff888ddc7faa292f46548
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-08-12 10:23:05 +0300

    Create part-z-0005-logic-comparision1.dart

---
+++ part-z-0005-logic-comparision1.dart
@@ -0,0 +1,21 @@
+void main() {
+
+    bool x = true;
+
+    bool y = false;
+
+    bool z = true;
+
+    print(x == y);
+  
+    print(y != z);
+  
+    print(x !=y && y == z);
+  
+    print(x == z || y != z);
+}
+
+// false
+// true
+// false
+// true

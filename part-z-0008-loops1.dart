aaf8b2bf3f81deb76f579050ba9d597403003cce
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-08-13 10:16:05 +0300

    Create part-z-0008-loops1.dart

---
+++ part-z-0008-loops1.dart
@@ -0,0 +1,16 @@
+void main() {
+  
+  for (int i = 25; i > -10; i -= 5) {
+    
+    print("i value is $i");
+  }
+
+}
+
+// i value is 25
+// i value is 20
+// i value is 15
+// i value is 10
+// i value is 5
+// i value is 0
+// i value is -5

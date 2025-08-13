6a87e498368d70df3e31f375a4898685cb368dbe
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-08-13 10:05:16 +0300

    Create part-z-0006-conditionals1.dart

---
+++ part-z-0006-conditionals1.dart
@@ -0,0 +1,20 @@
+void main() {
+  
+  int x = 10;
+
+  int y = 3;
+  
+  if (x > y && x == 8) {
+    
+    print("Lorem ipsum");
+    
+  } else if (x != y || y > 5) {
+    
+    print("Ceteris paribus");
+    
+  } else {
+    
+    print("Carpe diem");
+  }
+  
+}

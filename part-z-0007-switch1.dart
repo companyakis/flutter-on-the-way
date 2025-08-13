eaf014e9b9e7a3b6d6853a2997ceeadc1931b8ca
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2025-08-13 10:11:38 +0300

    Create part-z-0007-switch1.dart

---
+++ part-z-0007-switch1.dart
@@ -0,0 +1,24 @@
+void main() {
+  
+  int selection = 2;
+
+  switch (selection) {
+      
+    case 1:
+      print("Out of sight, out of mind");
+      break;
+
+    case 2:
+      print("A rolling stone gathers no moss");
+      break;
+    
+    case 3:
+      print("A barking dog never bites");
+      break;
+      
+    default:
+      print("Select 1, 2 or 3...");
+      break;
+  }
+  
+}
